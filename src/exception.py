8335b495273b19b312c66adcf027b646adb0329b
Priyansh143 <priyansh.kashyap25@gmail.com>
2024-09-22 23:30:54 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,8 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys): 
-    _ , _ , exc_tb = error_detail.exc_info()
+    _ , _ , exc_tb = error_detail.exc_info() #exc_tb is the point in the code where the exception was raised
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message
